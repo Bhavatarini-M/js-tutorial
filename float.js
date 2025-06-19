8b1e60710c6ed3857116a1fcbb5a2c87c3d19113
Bhavatarini-M <bhavatarinimohan13@gmail.com>
2025-06-19 10:54:42 +0530

    complete operators topic

---
+++ float.js
@@ -0,0 +1,10 @@
+let num1 = 2.1
+let num2 = 5.1
+
+console.log(num1 + num2);
+
+let result = Math.round((21+51) / 10).toFixed(2);
+console.log(result);
+
+let result1 = (2.1+5.1).toFixed(2);
+console.log(result1);
